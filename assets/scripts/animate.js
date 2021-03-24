c4620ebe2e03c77c1958e497eb52b1b0bcb19284
Ron Paolo Toyhacao <rptoyhacao@gmail.com>
2021-03-24 16:44:35 +0800

    Added GSAP

---
+++ assets/scripts/animate.js
@@ -0,0 +1,40 @@
+gsap.from('#illustration1', {
+    scrollTrigger: '#illustration1',
+    y: 200,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+gsap.from('#illustration2', {
+    scrollTrigger: '#illustration2',
+    y: 200,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+gsap.from('#illustration3', {
+    scrollTrigger: '#illustration3',
+    y: 200,
+    duration: 1.5,
+    opacity: 0,
+    scrub: 1
+});
+
+
+
+
+
+
+
+
+// let tl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.feature-container',
+//         start: 'center bottom',
+//         end: 'center top'
+//     }
+// });
+
+// tl.from('#illustration1', { y: 200, opacity: 0, duration: 1.5})
+// tl.from('#illustration2', { y: 200, opacity: 0, duration: 1.5})
+// tl.from('#illustration3', { y: 200, opacity: 0, duration: 1.5})
